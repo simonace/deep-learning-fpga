ace2f21f3910d10b0d9075cd5880ef5527b5eb42
Bili Xu <xubili93@gmail.com>
2018-03-10 09:19:40 +0800

    Convert normal output and input ports to wires

--- src/hdl/mac/MACAcceleratorTop.v
+++ src/hdl/mac/MACAcceleratorTop.v
@@ -30,16 +30,16 @@ module MACAcceleratorTop #(
     input wire s_axi_rready,
 
     // Input (AXI-Stream Slave Interface)
-    input [DATA_WIDTH-1:0] s_axis_tdata,
-    input s_axis_tvalid,
-    input s_axis_tlast,
-    output s_axis_tready,
+    input wire [DATA_WIDTH-1:0] s_axis_tdata,
+    input wire s_axis_tvalid,
+    input wire s_axis_tlast,
+    output wire s_axis_tready,
 
     // Output (AXI-Stream Master Interface)
-    input m_axis_tready,
-    output [DATA_WIDTH-1:0] m_axis_tdata,
-    output m_axis_tvalid,
-    output m_axis_tlast
+    input wire m_axis_tready,
+    output wire [DATA_WIDTH-1:0] m_axis_tdata,
+    output wire m_axis_tvalid,
+    output wire m_axis_tlast
 );
 
     MACAccelerator #(
