02441dea173db8ae931d7504ea50d81daed78f11
Bili Xu <xubili93@gmail.com>
2018-03-15 21:38:27 +0800

    Create initialize memory script to load weights and inputs

---
+++ src/software/zynq/mac/tcl/init_mem.tcl
@@ -0,0 +1,6 @@
+mwr -bin -file /home/xbili/Code/keras-mnist/data/int32/l1_w.data 0x00200000 401408
+mwr -bin -file /home/xbili/Code/keras-mnist/data/int32/l2_w.data 0x00300000 5120
+mwr -bin -file /home/xbili/Code/keras-mnist/data/int32/l1_b.data 0x00400000 512
+mwr -bin -file /home/xbili/Code/keras-mnist/data/int32/l2_b.data 0x00480000 10
+mwr -bin -file /home/xbili/Code/keras-mnist/data/int32/x_test.data 0x00500000 7840000
+mwr -bin -file /home/xbili/Code/keras-mnist/data/int32/y_test.data 0x00700000 100000
