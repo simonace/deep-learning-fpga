f257ebcc96d0061e945335bedb29675bd0a9942a
Bili Xu <xubili93@gmail.com>
2018-03-15 22:59:15 +0800

    Change memory address to load weights and inputs

--- src/software/zynq/mac/tcl/init_mem.tcl
+++ src/software/zynq/mac/tcl/init_mem.tcl
@@ -1,6 +1,6 @@
-mwr -bin -file /home/xbili/Code/keras-mnist/data/int32/l1_w.data 0x00200000 401408
-mwr -bin -file /home/xbili/Code/keras-mnist/data/int32/l2_w.data 0x00300000 5120
-mwr -bin -file /home/xbili/Code/keras-mnist/data/int32/l1_b.data 0x00400000 512
-mwr -bin -file /home/xbili/Code/keras-mnist/data/int32/l2_b.data 0x00480000 10
-mwr -bin -file /home/xbili/Code/keras-mnist/data/int32/x_test.data 0x00500000 7840000
-mwr -bin -file /home/xbili/Code/keras-mnist/data/int32/y_test.data 0x00700000 100000
+mwr -bin -file /home/xbili/Code/keras-mnist/data/int32/l1_w.data 0x00500000 401408
+mwr -bin -file /home/xbili/Code/keras-mnist/data/int32/l2_w.data 0x00600000 5120
+mwr -bin -file /home/xbili/Code/keras-mnist/data/int32/l1_b.data 0x00700000 512
+mwr -bin -file /home/xbili/Code/keras-mnist/data/int32/l2_b.data 0x00800000 10
+mwr -bin -file /home/xbili/Code/keras-mnist/data/int32/x_test.data 0x00900000 7840000
+mwr -bin -file /home/xbili/Code/keras-mnist/data/int32/y_test.data 0x00A00000 100000
